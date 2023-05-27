c8b23bc7b04c594acd46cbc0af21472f7984ecbf
rishadbaniya <rbdroid01@gmail.com>
2023-05-27 16:35:55 +0545

    DONE

--- index.js
+++ index.js
@@ -1,34 +1,86 @@
+document.body.style.border = "5px solid red";
+
+const fetch_data = (title, site) => {
+    return new Promise((resolve, reject) => {
+    fetch('http://localhost:9500/product_links', {
+        method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+      body: JSON.stringify({
+        title: title,
+        site: site
+      })
+    }) .then(response => {
+      return response.json();
+    })
+    .then(data => {
+        resolve(data);
+    })
+    .catch(_ => {
+        resolve([]);
+    })
+  });
+
+}
+window.onload = () => {
+    const currentUrl = new URL(window.location.href);
+    let site = currentUrl.hostname.split(".")[1];
+    let title  = null;
 
 
-const data = [
-  { name: "Product 1", image: "image1.jpg", price: "$10.00", description: "This is product 1" },
-  { name: "Product 2", image: "image2.jpg", price: "$15.00", description: "This is product 2" },
-  { name: "Product 3", image: "image3.jpg", price: "$20.00", description: "This is product 3" }
-];
 
-const resultsContainer = document.getElementById("results");
+    if (site === "daraz" ){
+        console.log("ITS DARAZ");
+        title = document.getElementsByClassName("pdp-mod-product-badge-title")[0].innerHTML;
+        console.log("THE TITLE IS : " + title);
+        let products_html = "";
+        fetch_data(title, site).then((data)=>{
+            if (data === []) {
+                products_html = "<h1>PriceWise : Sorry! I couldn't recommend you anything here</h1>";
+            }else {
+                for (let i = 0; i < data.length; i++){
+                    let product = data[i];
+                    let html = String.raw`
+                        <div class="product" style="display: flex; align-items: center;">
+                          <img src="${product.image_link}" alt="Product Image" height="100" width="100" class="product-image" style="display : block;border-radius : 8px; padding : 8px;">
+                          <div class="product-info">
+                            <h3 class="product-name">${product.name}</h3>
+                            <p class="product-price">${product.price}</p>
+                            <p class="product-site">Available at <a href="${product.link}">${product.website}</a></p>
+                          </div>
+                        </div>
+                    `;
+                    console.log(html);
+                    products_html += html;
+                }
+                console.log(products_html);
+                let custom_recommendation_elem = document.createElement("div");
+                custom_recommendation_elem.innerHTML = "<h1>PriceWise Recommendations!</h1>"
+                custom_recommendation_elem.innerHTML += products_html;
+
+                let block = document.querySelector(".pdp-block.pdp-block__main-information");
+                block.parentNode.insertBefore(custom_recommendation_elem, block.nextSibling)
+            }
+        }).catch((_)=>{
+
+        });
+        
+    }else if (site === "hamrobazaar"){
+
+    }else if (site === "sastodeal") {
+
+    }else{
+        custom_recommendation_elem.innerHTML = "<h1>PriceWise : Sorry! I couldn't recommend you anything here</h1>";
+    }
 
 
-data.forEach(product => {
-  const productDiv = document.createElement("div");
-  productDiv.className = "product";
 
-  const nameElement = document.createElement("h2");
-  nameElement.textContent = product.name;
+    if (title !== null){
+//
+//    console.log(title);
 
-  const imageElement = document.createElement("img");
-  imageElement.src = product.image;
 
-  const priceElement = document.createElement("p");
-  priceElement.textContent = product.price;
+    }
+}
 
-  const descriptionElement = document.createElement("p");
-  descriptionElement.textContent = product.description;
-
-  productDiv.appendChild(nameElement);
-  productDiv.appendChild(imageElement);
-  productDiv.appendChild(priceElement);
-  productDiv.appendChild(descriptionElement);
-
-  resultsContainer.appendChild(productDiv);
-});
